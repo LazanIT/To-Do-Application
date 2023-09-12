c857a937314c307c936d2793afc05a318b2ffe4c
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-09-12 21:52:06 +0200

    Skelet for Script
    Taking id of elements, and writing first function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test");
+// console.log("test");
+
+const inputBox = document.querySelector("#input-box");
+const listGroup = document.querySelector("#list-group");
+const addTaskBtn = document.querySelector("#addTask");
+
+// console.log(inputBox);
+// console.log(listGroup);
+// console.log(addTaskBtn);
+
+addTaskBtn.addEventListener("click", addTask());
+
+const addTask = () => {
+    
+};
