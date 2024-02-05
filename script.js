751e772f3ccf2cfecaecbd2cd55e88da201edb9e
LazanIT <117667390+LazanIT@users.noreply.github.com>
2024-02-05 22:51:46 +0100

    Deleted all comments in script file

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// console.log("test");
-
 const inputBox = document.querySelector("#input-box");
 const listGroup = document.querySelector("#list-group");
 const addTaskBtn = document.querySelector("#addTask");
 
-// console.log(inputBox);
-// console.log(listGroup);
-// console.log(addTaskBtn);
 let i = 0;
 const addTask = () => {
-  // console.log("Succes");
-
   let alertText = document.querySelector("h5");
 
   let li = document.createElement("li");
